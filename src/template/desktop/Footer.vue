20709f7f3f24a3b360e1757d282310706cb5c60a
euclides <euclides.nascimento@vivitech.com.br>
2020-03-30 14:15:22 -0300

    Footer

--- src/template/desktop/Footer.vue
+++ src/template/desktop/Footer.vue
@@ -1,12 +1,58 @@
 <template>
   <b-container fluid>
-      <b-row>
-          <b-col md="12" style=" height:150px;width:100%" > asa</b-col>
-      </b-row>
+    <b-row>
+      <b-col md="1"></b-col>
+      <b-col md="2">
+        <img class="logo" src="~@/assets/logo_bottom.png" />
+      </b-col>
+      <b-col md="9">
+        <b-row>
+          <b-col md="12">
+            <img class="img_col_1" src="~@/assets/discord_icon.png" />
+            <img class="img_col_1" src="~@/assets/whatsapp_group_icon.png" />
+            <img class="img_col_1" src="~@/assets/face_group_icon.png" />
+            <img class="img_col_1" src="~@/assets/facebook_icon.png" />
+            <img class="img_col_1" src="~@/assets/instagram_icon.png" />
+            <img class="img_col_1" src="~@/assets/twitter_icon.png" />
+            <img class="img_col_1" src="~@/assets/youtube_icon.png" />
+          </b-col>
+        </b-row>
+        <b-row>
+          <b-col md="12">
+            <img class="img_col_2" src="~@/assets/contact_icon.png" />
+            <img class="img_col_2" src="~@/assets/mail_icon.png" />
+            <img class="img_col_2" src="~@/assets/home_icon.png" />
+          </b-col>
+        </b-row>
+      </b-col>
+    </b-row>
   </b-container>
 </template>
 
 <script>
-export default {
-};
+export default {};
 </script>
+<style scoped>
+.logo{
+  width: 150px;
+  justify-content: center;
+  align-items: center;
+  margin-top: 10px;
+}
+.img_col_1 {
+  width: 35px;
+  margin-left: 20px;
+  float: left;
+  margin-top: 3px;
+}
+.img_col_2 {
+  width: 35px;
+  margin-left: 20px;
+  float: left;
+  margin-top: 5px;
+}
+.footer {
+  height: 200px !important;
+  width: 100%;
+}
+</style>
